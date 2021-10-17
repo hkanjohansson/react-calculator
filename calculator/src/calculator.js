112bbd4ab29a8e6ab7cb0b1494dedb6911c47431
hkanjohansson <hj222je@student.lnu.se>
2021-10-17 17:44:04 +0200

    Update

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -1,17 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './button.css'
 
-const Button = () => {
+const Button = (props) => {
   return (
-    <button onClick={() => console.log('Hello World')}>Hello World</button>
+    <button onClick={() => console.log(props.text)}>{props.text}</button> // TODO: OnClick works here, but not in ButtonsDisplay??
   )
 }
 
-class Display extends React.Component {
+/**
+ * TODO: Set state of ResultDisplay by connecting to the ButtonsDisplay component??
+ * Or should the calculator component take care of updating the display?
+ */
+class ResultDisplay extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      result: 0
+    }
+  }
+  
+  render () {
+    return(
+    <div className="result">{this.state.result}</div>
+    )
+  }  
+}
+
+// TODO: -Add event handlers to add text to the result screen
+//       -Move code from render to renderButton and rewrite using loops
+class ButtonsDisplay extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  renderButton(i) {
+
+  }
+  
+  /**
+   * Write to ResultDisplay on click
+   */
+  handleClick() {
+    console.log('hej')
+  }
+
+  render() {
+    return(
+      <div>
+        <div>
+          <Button text={1} />
+          <Button text={2} />
+          <Button text={3} />
+          <Button text='+' />
+        </div>
+        
+        <div>
+          <Button text={4} />
+          <Button text={5} />
+          <Button text={6} />
+          <Button text='-' />
+        </div>
+        
+        <div>
+          <Button text={7}/>
+          <Button text={8}/>
+          <Button text={9}/>
+          <Button text='x' />
+        </div>
+
+        <div>
+          <Button text='.' />
+          <Button text={0}/>
+          <Button text='=' />
+          <Button text='/' />
+        </div>
+        
+      </div>
+    )
   }
 }
 
@@ -22,8 +91,13 @@ class Calculator extends React.Component {
 
   render () {
     return (
-      <Button />
+      <div>
+        <ResultDisplay  id='result'/>
+        <ButtonsDisplay />
+      </div>
+      
     )
   }
 }
+
 ReactDOM.render(<Calculator />, document.getElementById('root'))
