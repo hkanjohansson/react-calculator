067947c3c1cc5083a4ed2c18af3f1655d50563d3
hkanjohansson <hj222je@student.lnu.se>
2021-10-30 18:39:36 +0200

    Fixed writing to display and clearing the screen

--- calculator/src/components/calculator.js
+++ calculator/src/components/calculator.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import '../button.css'
+import '../calculator.css'
 
-function Button ({ setResultValue, buttonState }) {
+function Button({ setResultValue, buttonState }) {
   useEffect(() => {
-    setResultValue(buttonState)
+    setResultValue()
   }, [buttonState, setResultValue]) // What variables are these dependent on??
 
   const handleClick = () => {
-    setResultValue(buttonState)
+    if (buttonState === 'C') {
+      setResultValue([])
+    } else {
+      setResultValue(prev => [prev, buttonState])
+    } 
   }
-  
+
   return (
     <button onClick={handleClick}>{buttonState}</button>
   )
 }
 
-function ButtonGrid ({ setResultValue }) {
-  const [buttonState, setButtonState] = useState({ 
+function Keypad({ setResultValue }) {
+  const [buttonState, setButtonState] = useState({
     one: 1,
     two: 2,
     three: 3,
@@ -26,52 +30,75 @@ function ButtonGrid ({ setResultValue }) {
     seven: 7,
     eight: 8,
     nine: 9,
-    zero: 0
+    zero: 0,
+    plus: '+',
+    minus: '-',
+    mult: 'x',
+    divide: '/',
+    dot: '.',
+    equals: '=',
+    clear: 'C'
   })
 
   return (
     <div>
       <div>
-        <Button setResultValue={setResultValue} buttonState={buttonState.one}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.two}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.three}/>
-      </div>
-      
-      <div>
-        <Button setResultValue={setResultValue} buttonState={buttonState.four}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.five}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.six}/>
+        <Button setResultValue={setResultValue} buttonState={buttonState.one} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.two} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.three} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.plus} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.clear} />
       </div>
 
       <div>
-        <Button setResultValue={setResultValue} buttonState={buttonState.seven}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.eight}/>
-        <Button setResultValue={setResultValue} buttonState={buttonState.nine}/>
+        <Button setResultValue={setResultValue} buttonState={buttonState.four} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.five} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.six} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.minus} />
+      </div>
+
+      <div>
+        <Button setResultValue={setResultValue} buttonState={buttonState.seven} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.eight} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.nine} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.mult} />
+      </div>
+
+      <div>
+        <Button setResultValue={setResultValue} buttonState={buttonState.dot} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.zero} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.zero} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.divide} />
+        <Button setResultValue={setResultValue} buttonState={buttonState.equals} />
       </div>
     </div>
   )
 }
 
-function ResultDisplay (props) {
-  const [resultValue, setResultValue] = useState(props.value)
+function ResultDisplay() {
+  const [resultValue, setResultValue] = useState([])
 
+  console.log(resultValue)
   return (
     <div>
       <div className='result-display'>{resultValue}</div>
-      <ButtonGrid setResultValue={setResultValue}/>
+      <Keypad setResultValue={setResultValue} />
     </div>
 
   )
 }
 
-function Calculator () {
+/**
+ * Function to handle calculator logic
+ * 
+ * @returns <ResultDisplay>
+ */
+function Calculator() {
   return (
     <div>
-      <ResultDisplay value={0} />
+      <ResultDisplay />
     </div>
   )
 }
 
 export default Calculator
-
-
